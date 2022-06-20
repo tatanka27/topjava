e0c0a8bdc62c428295fe598b3d2429805a29f2f5
Tatiana Naumovets <Tatiana_Naumovets@epam.com>
2022-06-20 12:32:18 +0300

    Add initialization table meals

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -1,5 +1,6 @@
 DROP TABLE IF EXISTS user_roles;
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS meals;
 DROP SEQUENCE IF EXISTS global_seq;
 
 CREATE SEQUENCE global_seq START WITH 100000;
@@ -23,3 +24,14 @@ CREATE TABLE user_roles
     CONSTRAINT user_roles_idx UNIQUE (user_id, role),
     FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
+
+CREATE TABLE meals
+(
+    id               INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+    dateTime         TIMESTAMP           DEFAULT now() NOT NULL,
+    description      VARCHAR                           NOT NULL,
+    calories         INTEGER             DEFAULT 0     NOT NULL,
+    user_id          INTEGER                           NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+);
+CREATE UNIQUE INDEX meals_unique_dateTime_idx ON meals (dateTime);
--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,5 +1,6 @@
 DELETE FROM user_roles;
 DELETE FROM users;
+DELETE FROM meals;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
 INSERT INTO users (name, email, password)
@@ -10,3 +11,13 @@ VALUES ('User', 'user@yandex.ru', 'password'),
 INSERT INTO user_roles (role, user_id)
 VALUES ('USER', 100000),
        ('ADMIN', 100001);
+
+INSERT INTO meals (datetime, description, calories, user_id)
+VALUES ('2022-02-01T09:00:00', 'Завтрак User', 400, 100000),
+       ('2022-02-01T15:00:00', 'Обед User', 600, 100000),
+       ('2022-02-01T19:00:00', 'Ужин User', 500, 100000);
+
+INSERT INTO meals (datetime, description, calories, user_id)
+VALUES ('2022-02-01T10:00:00', 'Второй завтрак Admin', 400, 100001),
+       ('2022-02-01T18:00:00', 'Бранч Admin', 600, 100001),
+       ('2022-02-02T19:00:00', 'Обед Admin', 2500, 100001);
